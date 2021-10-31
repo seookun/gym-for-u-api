bb0a02ff72fbc3bc692b71294fe9b73cef74a5f4
seookun <jseokh@gmail.com>
2021-10-31 18:08:49 +0900

    feat: create jwt util

---
+++ src/utils/jwt.ts
@@ -0,0 +1,49 @@
+import * as jwt from 'jsonwebtoken';
+import { HttpError } from 'routing-controllers';
+import Locals from '@/providers/Locals';
+import { UserRole } from '@/models/UserModel';
+
+type Payload = {
+  userId: string;
+};
+
+export type RefreshTokenPayload = Payload;
+
+export type AccessTokenPayload = {
+  roles: UserRole[];
+} & Payload;
+
+function sign(payload: Payload, secretKey: string, expiresIn: string | number) {
+  return jwt.sign(payload, secretKey, { expiresIn });
+}
+
+function verify(token: string, secretKey: string) {
+  try {
+    return <Payload>jwt.verify(token, secretKey);
+  } catch {
+    throw new HttpError(401, 'Invalid token.');
+  }
+}
+
+export function createRefreshToken(payload: RefreshTokenPayload) {
+  return sign(payload, Locals.refreshTokenSecretKey, Locals.refreshTokenExpiresIn);
+}
+
+export function verifyRefreshToken(token: string) {
+  return <RefreshTokenPayload>verify(token, Locals.refreshTokenSecretKey);
+}
+
+export function createAccessToken(payload: AccessTokenPayload) {
+  return sign(payload, Locals.accessTokenSecretKey, Locals.accessTokenExpiresIn);
+}
+
+export function verifyAccessToken(token: string) {
+  return <AccessTokenPayload>verify(token, Locals.accessTokenSecretKey);
+}
+
+export default {
+  createRefreshToken,
+  verifyRefreshToken,
+  createAccessToken,
+  verifyAccessToken,
+};
