92a4e3fe8f8f1bad8a6bde860cb96f0693bb417e
seookun <jseokh@gmail.com>
2021-10-31 18:10:17 +0900

    fix

--- src/models/GymModel.ts
+++ src/models/GymModel.ts
@@ -22,39 +22,38 @@ export interface Gym extends Document {
   updatedAt: Date;
 }
 
-export default model<Gym>(
-  'Gym',
-  new Schema<Gym>(
-    {
-      name: { type: String, required: true, unique: true },
-      description: { type: String },
-      thumbnail: { type: String },
-      homepage: { type: String },
-      address: new Schema<Address>(
-        {
-          zipno: { type: String, required: true },
-          main: { type: String, required: true },
-          detail: { type: String },
-        },
-        { _id: false },
-      ),
-      location: new Schema<Location>(
-        {
-          latitude: { type: Number, required: true },
-          longitude: { type: Number, required: true },
-        },
-        { _id: false },
-      ),
-    },
-    {
-      timestamps: true,
-      toJSON: {
-        versionKey: false,
-        transform(doc, ret) {
-          ret._id = ret._id.toString();
-          return ret;
-        },
+export const gymSchema = new Schema<Gym>(
+  {
+    name: { type: String, required: true, unique: true },
+    description: { type: String },
+    thumbnail: { type: String },
+    homepage: { type: String },
+    address: new Schema<Address>(
+      {
+        zipno: { type: String, required: true },
+        main: { type: String, required: true },
+        detail: { type: String },
+      },
+      { _id: false },
+    ),
+    location: new Schema<Location>(
+      {
+        latitude: { type: Number, required: true },
+        longitude: { type: Number, required: true },
+      },
+      { _id: false },
+    ),
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      versionKey: false,
+      transform(doc, ret) {
+        ret._id = ret._id.toString();
+        return ret;
       },
     },
-  ),
+  },
 );
+
+export default model<Gym>('Gym', gymSchema);
